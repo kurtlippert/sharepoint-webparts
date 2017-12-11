78d62dd0b5fa957b6cb0b62e28b0d58f30ed7abd
Kurt B Lippert <kblippert@johnstoncc.edu>
2017-12-11 10:32:36 -0500

    added typestyle (with helpers), and added styles to hello world. Included the 'serve' npm script. This unsets the http2 env var. Leaving this alone prevents the app from running

--- src/webparts/helloWorld/components/HelloWorld.ts
+++ src/webparts/helloWorld/components/HelloWorld.ts
@@ -1,9 +1,53 @@
-import * as React from 'react';
-import styles from './HelloWorld.module.scss';
-import IHelloWorldWebPartProps from './IHelloWorldWebPartProps';
-import { escape } from '@microsoft/sp-lodash-subset';
+import * as React from 'react'
+import styles from './HelloWorld.module.scss'
+import IHelloWorldWebPartProps from './IHelloWorldWebPartProps'
+import { escape } from '@microsoft/sp-lodash-subset'
+
+import { style } from 'typestyle'
+import { padding, margin } from 'csstips'
 
 const r = React.createElement;
+
+// const helloWorldContainer = style({
+//   maxWidth: '700px',
+//   margin: '0px auto',
+//   boxShadow: '0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 25px 50px 0 rgba(0, 0, 0, 0.1)'
+// })
+
+// const helloWorldRow = style({
+
+// })
+
+const listStyle = style(
+  padding(10),
+  margin(10),
+  {
+    color: '#333333',
+    fontFamily: `'Segoe UI Regular WestEuropean', 'Segoe UI', Tahoma, Arial, sans-serif`,
+    fontSize: '14px',
+    fontWeight: 'normal',
+    boxSizing: 'border-box',
+    lineHeight: '50px',
+    listStyleType: 'none',
+    boxShadow: '0 4px 4px 0 rgba(0, 0, 0, 0.2), 0 25px 50px 0 rgba(0, 0, 0, 0.1)'
+})
+
+const listItemStyle = style(
+  margin(0),
+  padding(0),
+  padding(9, 28, 3, 28),
+  {
+    color: '#333333',
+    verticalAlign: 'center',
+    fontFamily: `'Segoe UI Regular WestEuropean', 'Segoe UI', Tahoma, Arial, sans-serif`,
+    fontSize: '14px',
+    fontWeight: 'normal',
+    boxSizing: 'border-box',
+    boxShadow: 'none',
+    zoom: 1,
+    position: 'relative'
+  }
+)
 
 const HelloWorld = ({ description, test, pageContext }: IHelloWorldWebPartProps) =>
   r('div', { className: styles.helloWorld }, [
@@ -23,4 +67,4 @@ const HelloWorld = ({ description, test, pageContext }: IHelloWorldWebPartProps)
     ])
   ])
 
-export default HelloWorld;
+export default HelloWorld
